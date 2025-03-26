02a706b4634c203faf77e61faba9c90664114bb6
Luvjeet Singh <luvjeet.singh@ayuvya.com>
2025-03-27 00:08:38 +0530

    feat: Made gitsigns load on bufnr

--- lua/thecode/lazy_plugins/gitsigns.lua
+++ lua/thecode/lazy_plugins/gitsigns.lua
@@ -1,29 +1,33 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        local gitsigns = require("gitsigns")
-        gitsigns.setup({
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-            current_line_blame = false,
-            on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-                map('n', '<leader>tb', gs.toggle_current_line_blame)
-                map('n', '<leader>td', gs.toggle_deleted)
-            end
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gitsigns = require("gitsigns")
+    gitsigns.setup({
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      current_line_blame = false,
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-})
-    end
+        vim.defer_fn(function()
+          gs.toggle_current_line_blame()
+        end, 100) -- Adding a small delay to ensure gitsigns is fully initialized
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>td', gs.toggle_deleted)
+      end
+
+    })
+  end
 }
-
-
