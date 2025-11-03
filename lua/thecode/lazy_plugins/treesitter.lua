6cef0c4958ec62000f474d6afb74638c9dd39ccc
Luvjeet Singh <luvjeet.singh@ayuvya.com>
2025-11-03 11:23:48 +0530

    feat: Breaking changes

--- lua/thecode/lazy_plugins/treesitter.lua
+++ lua/thecode/lazy_plugins/treesitter.lua
@@ -1,24 +1,19 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build=":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
 
-    config = function()
+  config = function()
+    require 'nvim-treesitter.configs'.setup({
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript" },
 
-require 'nvim-treesitter.configs'.setup ({
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript" },
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-    autotag = {
-        enable = true,
-    },
-
-    highlight = {
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+      highlight = {
         enable = true,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -26,21 +21,20 @@ require 'nvim-treesitter.configs'.setup ({
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
-    },
-})
-require 'nvim-treesitter.configs'.setup ({
-    textobjects = {
-        move = {
-            enable = true,
-            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        },
-    },
-})
-    end
+      },
+    })
+    -- require 'nvim-treesitter.configs'.setup({
+    --   textobjects = {
+    --     move = {
+    --       enable = true,
+    --       set_jumps = true,
+    --       goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+    --       goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+    --       goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+    --       goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+    --     },
+    --   },
+    -- })
+    require('nvim-ts-autotag').setup()
+  end
 }
-
-
-
